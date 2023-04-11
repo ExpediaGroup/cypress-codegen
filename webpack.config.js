15daa0ac319ec30e5f7e51c5a70ca0de63bc5c0f
Dan Adajian <dadajian@expediagroup.com>
2023-04-11 07:08:40 -0500

    simplify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
-const path = require('path');
-
 module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [{ test: /\.ts|\.tsx$/, use: 'ts-loader' }]
   }
