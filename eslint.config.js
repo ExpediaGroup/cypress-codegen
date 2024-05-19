3d71bedd01b1e2d92e12941713a49f5296e36296
Dan Adajian <dadajian@expediagroup.com>
2024-05-19 11:47:36 -0400

    initial

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+import typescriptEslint from "typescript-eslint";
+import cypressEslintPlugin from "eslint-plugin-cypress";
+
+export default [
+  ...typescriptEslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptEslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint.plugin,
+      "sort-annotation": cypressEslintPlugin,
+    },
+    rules: {
+      "no-console": "error",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "@typescript-eslint/no-unsafe-argument": "error",
+      "@typescript-eslint/no-unsafe-call": "error",
+    },
+  },
+  {
+    ignores: ["dist", "node_modules"],
+  },
+];
