64b7d9eba89d3dfeb870ac618f5b6944619c8174
Dan Adajian <dadajian@expediagroup.com>
2025-02-09 13:51:35 -0600

    fix

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { mount } from "cypress/react18";
+import { mount } from "cypress/react";
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
